0392684a9e9773705917377ed064442390e51566
Sam Lee <sflee36@yahoo.com.au>
2020-09-25 00:01:03 +0800

    art display block css and arrangement adjusted

--- src/app/gallery/browse/browse.component.ts
+++ src/app/gallery/browse/browse.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ArtworkService } from 'src/app/artwork.service';
+import { GalleryService } from 'src/app/gallery.service';
+import { ArtModel } from '../art.model';
 
 @Component({
   selector: 'app-browse',
@@ -8,40 +10,50 @@ import { ArtworkService } from 'src/app/artwork.service';
 })
 export class BrowseComponent implements OnInit {
 
-  artists: string[];
-  countries: string[];
-  genres: string[];
-  years: number[];
+  artists: {val: string, amount: number}[] = [];
+  countries: {val: string, amount: number}[] = [];
+  genres: {val: string, amount: number}[] = [];
+  years: {val: number, amount: number}[] = [];
   
   @Output() onClose = new EventEmitter<void>();
   @Output() filterQuery = new EventEmitter<{cat: string, val: string}>();
 
   displayEntryCount: boolean = false;
   
-  constructor(private artWorkService: ArtworkService) { }
+  constructor(private artWorkService: ArtworkService,
+    private galleryService: GalleryService) { }
 
   ngOnInit(): void {
     this.artWorkService.getCategoryList("artist").subscribe(
       (data: string[]) => {
-        this.artists = data;
+        data.forEach(element => {
+          this.artists.push({val: element, amount: 0});
+        });
       }
     );
+    console.log(this.artists)
 
     this.artWorkService.getCategoryList("genre").subscribe(
       (data: string[]) => {
-        this.genres = data;
+        data.forEach(element => {
+          this.genres.push({val: element, amount: 0});
+        });
       }
     );
 
     this.artWorkService.getCategoryList("country").subscribe(
       (data: string[]) => {
-        this.countries = data;
+        data.forEach(element => {
+          this.countries.push({val: element, amount: 0});
+        });
       }
     );
 
     this.artWorkService.getCategoryList("year").subscribe(
       (data: number[]) => {
-        this.years = data;
+        data.forEach(element => {
+          this.years.push({val: element, amount: 0});
+        });
       }
     );
   }
@@ -61,7 +73,42 @@ export class BrowseComponent implements OnInit {
     this.onClose.emit();
   }
 
-  calculateDisplayEntryCount() {
+  resetFilters(): void {
+    
+  }
 
+  calculateDisplayEntryCount(): void {
+    this.artists.forEach(element => {
+      this.artWorkService.filterByCategory("artist", element.val).subscribe(
+        (data: ArtModel[]) => {
+          element.amount = data.length;
+        }
+      )
+    });
+    
+    this.genres.forEach(element => {
+      this.artWorkService.filterByCategory("genre", element.val).subscribe(
+        (data: ArtModel[]) => {
+          element.amount = data.length;
+        }
+      )
+    });
+
+    this.countries.forEach(element => {
+      this.artWorkService.filterByCategory("country", element.val).subscribe(
+        (data: ArtModel[]) => {
+          element.amount = data.length;
+        }
+      )
+    });
+
+    this.years.forEach(element => {
+      this.artWorkService.filterByCategory("year", element.val.toFixed()).subscribe(
+        (data: ArtModel[]) => {
+          element.amount = data.length;
+        }
+      )
+    });
+    this.displayEntryCount = this.displayEntryCount === true ? false : true;
   }
 }
